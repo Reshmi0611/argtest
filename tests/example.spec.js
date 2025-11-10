0d6dfc95d0d0767527bb48098241bd974c26dcfa
Reshmi <Reshmi.M.R@gds.ey.com>
2025-11-10 08:37:57 +0530

    test455

--- tests/example.spec.js
+++ tests/example.spec.js
@@ -19,6 +19,6 @@ import { test, expect } from '@playwright/test';
 // });
 
 test('get started link1', async ({ page }) => {
-  await page.goto('http://localhost:3000/index.html');
+  await page.goto('https://reshmi0611.github.io/simple-website-template1/');
   console.log("print");
 });
