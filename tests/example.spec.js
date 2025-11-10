68c81b4e4e9f867bec3bad2c290612157d5246c6
Reshmi <Reshmi.M.R@gds.ey.com>
2025-11-10 07:01:56 +0530

    chk msg

--- tests/example.spec.js
+++ tests/example.spec.js
@@ -22,4 +22,5 @@ test('get started link1', async ({ page }) => {
   await page.goto('http://127.0.0.1:5500/lgtest/index.html');
   console.log("print");
   console.log("test");
+  console.log("check");
 });
